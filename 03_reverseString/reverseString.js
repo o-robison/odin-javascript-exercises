ea753b49933a6877d4a4e2dc80a4e2b37e0d4e3d
Owen Robison <orobison@proton.me>
2025-01-31 13:51:16 -0500

    03 solution, string reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let endPosition  = originalString.length-1;
+    let newString = "";
+    for (let i=endPosition; i>-1; i--){
+        newString += originalString[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
