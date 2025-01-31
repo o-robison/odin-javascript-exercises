f2c620beaaaeda9ea245bb023559f3ae156f5b32
Owen Robison <orobison@proton.me>
2025-01-31 13:42:19 -0500

    02 solution, string repeating

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatNum) {
+    if (repeatNum < 0) return "ERROR";
+    let newWord = "";
+    for (let i=0; i<repeatNum; i++){
+        newWord += word;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
