92d1a82107cd8958b9156aa85486786d5d9c19fc
Owen Robison <orobison@proton.me>
2025-01-31 15:11:30 -0500

    07 solution temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  var celsius = (farenheit-32) * (5/9);
+  celsius = Math.round(celsius*10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  var farenheit = (celsius*(9/5))+32;
+  farenheit = Math.round(farenheit*10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
