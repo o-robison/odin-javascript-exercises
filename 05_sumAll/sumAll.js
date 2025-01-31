fadd14af6e68e10f980ec8044beae8a73d7058a1
Owen Robison <orobison@proton.me>
2025-01-31 14:47:14 -0500

    05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne<0 || numTwo<0) return "ERROR";
+    if ((typeof numOne != "number") || (typeof numTwo != "number")) return "ERROR";
+    if ((Math.floor(numOne) != numOne) || (Math.floor(numTwo) != numTwo)) return "ERROR";
 
+    if (numOne > numTwo) {
+        let temp = numOne;
+        numOne = numTwo;
+        numTwo = temp;
+    }
+
+    let result = 0;
+    for (let i=numOne; i<(numTwo+1); i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
