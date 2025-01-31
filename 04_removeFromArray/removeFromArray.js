67f0307f425325032957ca7c55e274c7e5ee30bc
Owen Robison <orobison@proton.me>
2025-01-31 14:33:10 -0500

    04 array deletion

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    let removeNum = arguments.length;
+    let newArray = [];
+    for (element of inputArray){
+        let addElement = true;
+        for (let i=1; i<removeNum; i++){
+            if (arguments[i]===element) {
+                addElement = false;
+            }
+        }
+        if (addElement) newArray.push(element);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
