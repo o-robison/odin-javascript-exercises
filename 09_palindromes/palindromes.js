a7b40b03d59e07d988d22cba9bdb84051b5ece36
Owen Robison <orobison@proton.me>
2025-02-01 12:14:36 -0500

    09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    inputStr = inputStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    inputStr = inputStr.replace(/\s/g, "");
+    inputStr = inputStr.toLowerCase();
+    const inputLen = inputStr.length;
+    let newStr = "";
+    for (let i=inputLen-1; i>-1; i--){
+        newStr += inputStr[i];
+    }
+    if (inputStr==newStr) return true;
+    else return false;
 };
 
 // Do not edit below this line
