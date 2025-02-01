a36b8805f5dd498ffc2de4d06e69027105bd964d
Owen Robison <orobison@proton.me>
2025-02-01 12:35:41 -0500

    10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(typeof(a)==="string") a = parseInt(a);
+    if(a<0) return "OOPS";
+    if(a===0) return 0;
+    var addOne = 1;
+    var addTwo = 1;
+    if (a===1||a===2) return addOne;
+    for(i=2; i<a; i++){
+        var addThree = addOne + addTwo;
+        addOne = addTwo;
+        addTwo = addThree;
+    }
+    return addTwo;
 };
 
 // Do not edit below this line
