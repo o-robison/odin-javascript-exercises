7a83f653060ec6e0e9cdd93eb86fb2c312ad10b2
Owen Robison <orobison@proton.me>
2025-02-01 11:50:12 -0500

    08 solution calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let result = 0;
+  for (number of numbers) {
+    result += number;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = numbers[0];
+  for (let i=1; i<numbers.length; i++) {
+    result *= numbers[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a===0) return 1;
+  var result = a;
+  for (var i=a-1; i>0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
