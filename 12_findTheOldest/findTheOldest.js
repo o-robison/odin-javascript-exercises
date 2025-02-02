d67ea265430351568f792f86a89c9972056e2c51
Owen Robison <orobison@proton.me>
2025-02-02 15:14:46 -0500

    12 solution, reduce array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAge = function(birthYear, deathYear) {
+    if (!deathYear) deathYear = new Date().getFullYear();
+    return deathYear - birthYear;
+};
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return (currentAge > oldestAge) ? current : oldest;
+    });
 };
 
 // Do not edit below this line
